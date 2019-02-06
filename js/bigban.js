4fdc43dd6af20d69accafd7bb93899ae0b1aa47c
mlin.debian <491601759@qq.com>
2019-02-06 10:32:55 -0500

    new index.html

---
+++ js/bigban.js
@@ -0,0 +1,20 @@
+/* =Main INIT Function
+-------------------------------------------------------------- */
+function initializeSite() {
+	"use strict";
+
+    // Init effect 
+    $('#scene').parallax();
+
+
+
+
+
+};
+
+$(document).ready(function(){
+
+	initializeSite();
+
+
+});
